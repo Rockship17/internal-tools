3fc3cbcd75ae25fb903470b6ce1f956c50f34f0a
wolf4dev <phuocnguyencloud9@gmail.com>
2025-04-11 16:08:29 +0700

    fix build fail

--- src/components/admin/form-request/RequestHistory.tsx
+++ src/components/admin/form-request/RequestHistory.tsx
@@ -46,12 +46,6 @@ const mockRequests: Request[] = [
   },
 ]
 
-const statusColors = {
-  pending: "bg-yellow-100 text-yellow-800",
-  approved: "bg-green-100 text-green-800",
-  rejected: "bg-red-100 text-red-800",
-}
-
 const statusText = {
   pending: "Pending",
   approved: "Approved",
